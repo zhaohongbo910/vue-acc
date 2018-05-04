7e8aae682fdbeb56c70424c9854fd5ef7e774daf
zhaohb <569078391@qq.com>
2018-05-04 18:06:03 +0800

    add

---
+++ src/App.vue
@@ -0,0 +1,275 @@
+<style>
+@import './assets/common.css';
+</style>
+
+<template>
+  <router-view id="app" @login="loginDirect" @logout="logoutDirect"></router-view>
+</template>
+
+<script>
+import Vue from 'vue';
+import instance from './api';
+import userPath from './router/fullpath';
+import * as util from './assets/util.js';
+
+//请求拦截句柄
+let myInterceptor;
+
+export default {
+  data() {
+    return {
+      menuData: null,
+      userData: null,
+      aaaa:'',
+    }
+  },
+
+  methods: {
+    //  获得权限
+    /**
+     *    delete,/account/**:true
+          delete,/role/*:true
+          get,/accounts:true
+          get,/roles:true
+          get,/signin:true
+          post,/account/*\/roles:true
+          post,/role:true
+          put,/account/:true
+          put,/role/**:true      
+     * 
+     * 
+     */
+
+    getPermission: function(userInfo) {
+      let resourcePermission = {};
+      if (Array.isArray(userInfo.resources)) {
+        userInfo.resources.forEach(function(e, i) {
+          let key = e.method.toLowerCase() + ',' + e.url;
+          resourcePermission[key] = true;
+        });
+      }
+      console.log(resourcePermission,'????????????????????????????????')
+      return resourcePermission;
+    },
+    // 权限请求拦截
+    setInterceptor: function(resourcePermission) {
+      let vm = this;
+
+      myInterceptor = instance.interceptors.request.use(function(config) {
+        console.log(config,'*********************************')
+        //得到请求路径
+        let perName = config.url.replace(config.baseURL, '').replace('/GET','').replace('/POST','').split('?')[0];
+        //权限格式1 /path/${param}
+        let reg1 = perName.match(/^(\/[^\/]+\/)[^\/]+$/);
+        if (reg1) {
+          perName = reg1[1] + '**';
+        }
+        //权限格式2 /path/${param}/path
+        let reg2 = perName.match(/^\/[^\/]+\/([^\/]+)\/[^\/]+$/);
+        if (reg2) {
+          perName = perName.replace(reg2[1], '*');
+        }
+        console.log(perName,'/////////////////////////////////////')
+        //校验权限
+        console.log(resourcePermission[config.method + ',' + perName],'666666666666666666666666666')
+        if (!resourcePermission[config.method + ',' + perName]) {
+          //调试信息
+          console.warn(resourcePermission, config.method + ',' + perName);
+          vm.$message({
+            message: '无访问权限，请联系企业管理员',
+            type: 'warning'
+          });
+          //拦截请求
+          return Promise.reject({
+            message: 'no permission'
+          });
+        }
+
+        return config;
+      });
+    },
+    getRoutes: function(userInfo) {
+      if(!userInfo.menus){
+        return console.warn(userInfo);
+      }
+      let vm = this;
+      let allowedRouter = [];
+
+      //将菜单数据转成多维数组格式
+      let arrayMenus = util.buildMenu(userInfo.menus);
+      console.log(arrayMenus,'mmmmmmmmmmmmmmmmmmm')
+      //将多维数组转成对象格式
+      let hashMenus = {};
+      let setMenu2Hash = function(array, base) {
+        array.map(key => {
+          if (key.route) {
+            let hashKey = ((base ? base + '/' : '') + key.route).replace(/^\//, '');
+            hashMenus['/' + hashKey] = true;
+            if (Array.isArray(key.children)) {
+              setMenu2Hash(key.children, key.route);
+            }
+          }
+        });
+      };
+
+      setMenu2Hash(arrayMenus);
+      //全局挂载hashMenus，用于实现路由守卫
+      console.log(hashMenus,'hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh')
+      this.$root.hashMenus = hashMenus;
+
+      //筛选本地路由方法
+      let findLocalRoute = function(array, base) {
+        let replyResult = [];
+        array.forEach(function(route) {
+          let pathKey = (base ? base + '/' : '') + route.path;
+          if (hashMenus[pathKey]) {
+            if (Array.isArray(route.children)) {
+              route.children = findLocalRoute(route.children, route.path);
+            }
+            replyResult.push(route);
+          }
+        });
+        if (base) {
+          return replyResult;
+        } else {
+          allowedRouter = allowedRouter.concat(replyResult);
+        }
+      }
+      let originPath = util.deepcopy(userPath[0].children);
+      findLocalRoute(originPath);
+      return allowedRouter;
+    },
+    extendRoutes: function(allowedRouter) {
+      let vm = this;
+      let actualRouter = util.deepcopy(allowedRouter);
+      actualRouter.map(e => {
+        //复制子菜单信息到meta用于实现导航相关效果，非必需
+        if (e.children) {
+          if (!e.meta) e.meta = {};
+          e.meta.children = e.children;
+        }
+        //为动态路由添加独享守卫
+        return e.beforeEnter = function(to, from, next){
+          if(vm.$root.hashMenus[to.path]){
+            next()
+          }else{
+            next('/401')
+          }
+        }
+      });
+      let originPath = util.deepcopy(userPath);
+      originPath[0].children = actualRouter;
+      //注入路由
+      vm.$router.addRoutes(originPath.concat([{
+        path: '*',
+        redirect: '/404'
+      }]));
+    },
+    
+   /**
+    *    1获取用户的token  检查token 是否存在  如果不存在 跳转到登录页面
+    *    
+    *    2.设置请求头同意携带 token   获取用户的权限信息
+    * 
+    *    3.
+    *  
+    * 
+    * 
+    * 
+    */
+
+
+
+    signin: function(callback) {             
+      let vm = this;
+      //检查登录状态
+      let localUser = util.session('token');
+      if (!localUser || !localUser.token) {
+        return vm.$router.push({ path: '/login', query: { from: vm.$router.currentRoute.path } });
+      }
+
+      //设置请求头统一携带token
+      instance.defaults.headers.common['Authorization'] = 'Bearer ' + localUser.token;
+      //获取用户信息及权限数据
+      instance.get(`/signin`, {
+        params: {
+          Authorization: localUser.token
+        }
+      }).then((res) => {
+        console.log(res,'-----------------------------------')
+        let userInfo = res.data;
+        console.log(userInfo,'+++++++++++++++++++++++++++++++++')
+
+        //取得资源权限对象
+        let resourcePermission = vm.getPermission(userInfo);   
+
+
+        //使用资源权限设置请求拦截
+        vm.setInterceptor(resourcePermission);
+
+
+        //获得实际路由
+        let allowedRouter = vm.getRoutes(userInfo);
+        //若无可用路由限制访问
+        if (!allowedRouter || !allowedRouter.length) {
+          util.session('token','');
+          return document.body.innerHTML = ('<h1>账号访问受限，请联系系统管理员！</h1>');
+        }
+        //动态注入路由
+        vm.extendRoutes(allowedRouter);
+        //保存数据用作他处，非必需
+        vm.menuData = allowedRouter;
+        vm.userData = userInfo;
+        //权限检验方法
+        Vue.prototype.$_has = function(rArray) {
+          let resources = [];
+          let permission = true;
+          //提取权限数组
+          if (Array.isArray(rArray)) {
+            rArray.forEach(function(e) {
+              resources = resources.concat(e.p);
+            });
+          } else {
+            resources = resources.concat(rArray.p);
+          }
+          //校验权限
+          resources.forEach(function(p) {
+            if (!resourcePermission[p]) {
+              return permission = false;
+            }
+          });
+          return permission;
+        }
+        //执行回调
+        typeof callback === 'function' && callback();
+      })
+    },
+    // 登录管理
+    loginDirect: function(newPath){
+
+      this.signin(() => {
+        this.$router.replace({path: newPath || '/'});
+      });
+    },
+    
+    //退出管理
+    logoutDirect: function(){
+      //清除session
+      util.session('token','');
+      //清除请求权限控制
+      instance.interceptors.request.eject(myInterceptor);
+      //清除菜单权限
+      this.$root.hashMenus = {};
+      //清除请求头token
+      instance.defaults.headers.common['Authorization'] = '';
+      //回到登录页
+      this.$router.replace({path: '/login'});
+    }
+  },
+
+  created: function(newPath) {
+    this.signin();
+    console.log(this)
+  }
+}
+</script>
---
+++ src/api/goods.js
@@ -0,0 +1,33 @@
+import instance from './index';
+const preUrlPath = '';
+//查询药品
+const request = {
+  p: ['get,/medications'],
+  r: params => {
+    return instance.get(`${preUrlPath}/medications`, {params})
+  }
+};
+
+//批量删除药品
+const remove = {
+  p: ['delete,/medication/**'],
+  r: params => {
+    return instance.delete(`${preUrlPath}/medication/${params.id}`)
+  }
+};
+
+//越权请求
+const notAllowed = {
+  p: ['get,/roles/notAllowed'],
+  r: params => {
+    return instance.get(`${preUrlPath}/roles/notAllowed`, {params})
+  }
+}
+
+
+
+export {
+  request,
+  remove,
+  notAllowed
+}
